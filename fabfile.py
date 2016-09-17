3eea8fdb376acceef39b938afaa518c04b62d7cb
Eric Pascual <eric@pobot.org>
2016-09-17 17:28:09 +0200

    removed explicit host definition and added systemd service related commands

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,17 @@
 from pybot.fabtasks import *
 from fabric.state import output
 
-env.hosts = ['rpi3']
 output.output = False
+
+PREFIX = '/home/pi/.local'
+CMDE_BASE = "PYTHONPATH=%(prefix)s/lib/python2.7/site-packages %(prefix)s/bin/lcdfs-systemd-" % {'prefix': PREFIX}
+
+
+@task()
+def install_service():
+    sudo(CMDE_BASE + 'install', shell=True)
+
+
+@task()
+def remove_service():
+    sudo(CMDE_BASE + 'remove', shell=True)
